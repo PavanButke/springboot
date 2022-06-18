f3f1d336d532f254dc9b289944100c4fef68377b
PB67966 <PB67966@POOLW42SLPC7061.ind.zensar.com>
2022-06-18 19:45:02 +0530

    Fetching Static Products from List; Perform CRUD Ops over the static data. Tested Succesfully on Browser

--- Readme.md
+++ Readme.md
@@ -5,25 +5,52 @@ An Springboot Application For Product Service .
 ## Steps Involved
 * Creating an Entity
 * Created an Endpoints with the help @RequestMapping within Controller Class.
-
+*C
 
 ### Dependencies
 
 * Spring Web Services 
 * Spring Dev Tools
-* 
+* Code an Static List
+* Code methods for Getting all Product Records
+* Test all the EndPoints on PostMan
 
 ### Installing
 
 * Create Project in STS using New Project-> SpringBoot -> Select Specification -> Specify your java version
 * Any modifications needed to be made to files/folders
 
+
 ### Executing program
 
 * Run ProductServiceApplication as SpringBoot Application
-* Step-by-step bullets
+
 ```
-code blocks for commands
+Endpoints:
+	```
+	Getting all the Products from List
+	 http://localhost:8080/products/
+	```
+	```
+	Getting an specific record based on Product's Id
+	 http://localhost:8080/products/{productId}
+	 ex.
+	 http://localhost:8080/products/89
+	```
+	
+	```
+	Uploading new Product Record in the List
+	 http://localhost:8080/products/
+	 ex.
+	 	body of Postman-->
+	 	 {
+        "productId": 101,
+        "productName": "Cheeze",
+        "expDate": "28May2023"
+   		 }
+   		 
+   		Hit the Post on mentioned URI
+	```
 ```
 
 
--- src/main/java/com/assignment/controller/ProductController.java
+++ src/main/java/com/assignment/controller/ProductController.java
@@ -1,58 +1,124 @@
 package com.assignment.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.assignment.entity.Product;
 
 @RestController
 public class ProductController {
-	
-	public String myMethod() {
-		return "\r\n"
-				+ "<style>\r\n"
-				+ "    body {\r\n"
-				+ "        background-color: skyblue;\r\n"
-				+ "    }\r\n"
-				+ "    div {\r\n"
-				+ "        height: 200px;\r\n"
-				+ "        margin: 20px;\r\n"
-				+ "        border: 5px solid;\r\n"
-				+ "        background-color: yellow;\r\n"
-				+ "    }\r\n"
-				+ "</style>\r\n"
-				+ "<body>\r\n"
-				+ "    <div>\r\n"
-				+ "        <p>Welcome to Spring Web Tutorile</p>\r\n"
-				+ "\r\n"
-				+ "        <div>\r\n"
-				+ "            <p>I am Pavan</p>\r\n"
-				+ "        </div>\r\n"
-				+ "    </div>\r\n"
-				+ "</body>";
+
+	List<Product> products = new ArrayList<Product>();
+
+	public ProductController() {
+
+		products.add(new Product(89, "Peanut Butter", "18May2023"));
+		products.add(new Product(91, "Bread", "18May2022"));
+		products.add(new Product(89, "Jam", "18March2022"));
 	}
+
+	@RequestMapping("/products/{productId}")
 	
-	@RequestMapping("/check")
-	public String myMethod2() {
-		return "\r\n"
-				+ "<style>\r\n"
-				+ "    body {\r\n"
-				+ "        background-color: skyblue;\r\n"
-				+ "    }\r\n"
-				+ "    div {\r\n"
-				+ "        height: 200px;\r\n"
-				+ "        margin: 20px;\r\n"
-				+ "        border: 5px solid;\r\n"
-				+ "        background-color: yellow;\r\n"
-				+ "    }\r\n"
-				+ "</style>\r\n"
-				+ "<body>\r\n"
-				+ "    <div>\r\n"
-				+ "        <p>Welcome to Spring Web Tutorile</p>\r\n"
-				+ "\r\n"
-				+ "        <div>\r\n"
-				+ "            <p>I am Pavan</p>\r\n"
-				+ "        </div>\r\n"
-				+ "    </div>\r\n"
-				+ "</body>";
+	
+	public Product getProduct(@PathVariable("productId") int productId) {
 		
+		System.out.println("SS"+productId);
+		for (Product product : products) {
+			if (product.getProductId() == productId) {
+				return product;
+			}
+
+		}
+		return null;
+
 	}
+
+	@RequestMapping(value = { "/products", "/listOfProducts" }, method = RequestMethod.GET)
+	public List<Product> getAllProducts() {
+		return products;
+	}
+
+	@RequestMapping(value = "/products", method = RequestMethod.POST)
+	public void insertProduct(@RequestBody Product product) {
+		products.add(product);
+
+	}
+
+	@RequestMapping(value = "/products/{productId}", method = RequestMethod.PUT)
+	public void updateProduct(@PathVariable("productId") int productId, @RequestBody Product product) {
+		Product newProduct = getProduct(productId);
+		newProduct.setProductId(product.getProductId());
+		newProduct.setProductName(product.getProductName());
+		newProduct.setExpDate(product.getExpDate());
+
+		products.add(newProduct);
+	}
+
+	@RequestMapping(value = "/products/{productId}", method = RequestMethod.DELETE)
+	public void deleteProduct(@PathVariable("productId") int productId) {
+		for (int i = 0; i < products.size(); i++) {
+			Product product = products.get(i);
+			if (product.getProductId() == productId) {
+				products.remove(product);
+			}
+
+		}
+	}
+
+//	@RequestMapping("/")
+//	public String myMethod() {
+//		return "\r\n"
+//				+ "<style>\r\n"
+//				+ "    body {\r\n"
+//				+ "        background-color: skyblue;\r\n"
+//				+ "    }\r\n"
+//				+ "    div {\r\n"
+//				+ "        height: 200px;\r\n"
+//				+ "        margin: 20px;\r\n"
+//				+ "        border: 5px solid;\r\n"
+//				+ "        background-color: yellow;\r\n"
+//				+ "    }\r\n"
+//				+ "</style>\r\n"
+//				+ "<body>\r\n"
+//				+ "    <div>\r\n"
+//				+ "        <p>Welcome to Spring Web Tutorile</p>\r\n"
+//				+ "\r\n"
+//				+ "        <div>\r\n"
+//				+ "            <p>I am Pavan</p>\r\n"
+//				+ "        </div>\r\n"
+//				+ "    </div>\r\n"
+//				+ "</body>";
+//	}
+//	
+//	@RequestMapping("/check")
+//	public String myMethod2() {
+//		return "\r\n"
+//				+ "<style>\r\n"
+//				+ "    body {\r\n"
+//				+ "        background-color: skyblue;\r\n"
+//				+ "    }\r\n"
+//				+ "    div {\r\n"
+//				+ "        height: 200px;\r\n"
+//				+ "        margin: 20px;\r\n"
+//				+ "        border: 5px solid;\r\n"
+//				+ "        background-color: yellow;\r\n"
+//				+ "    }\r\n"
+//				+ "</style>\r\n"
+//				+ "<body>\r\n"
+//				+ "    <div>\r\n"
+//				+ "        <p>Welcome to Spring Web Tutorile</p>\r\n"
+//				+ "\r\n"
+//				+ "        <div>\r\n"
+//				+ "            <p>I am Pavan</p>\r\n"
+//				+ "        </div>\r\n"
+//				+ "    </div>\r\n"
+//				+ "</body>";
+//		
+//	}
 }
