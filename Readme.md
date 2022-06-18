efd1291d31738562622c99a367e1edb33e67e3d7
PB67966 <PB67966@POOLW42SLPC7061.ind.zensar.com>
2022-06-18 18:40:24 +0530

    Entity Developed

---
+++ Readme.md
@@ -0,0 +1,37 @@
+# ProducService Application
+
+An Springboot Application For Product Service .
+
+## Steps Involved
+* Creating an Entity
+
+
+
+### Dependencies
+
+* Spring Web Services 
+* Spring Dev Tools
+* 
+
+### Installing
+
+* Create Project in STS using New Project-> SpringBoot -> Select Specification -> Specify your java version
+* Any modifications needed to be made to files/folders
+
+### Executing program
+
+* Run ProductServiceApplication as SpringBoot Application
+* Step-by-step bullets
+```
+code blocks for commands
+```
+
+
+
+
+## Authors
+
+Pavan Butke
+
+
+## Acknowledgments
---
+++ src/main/java/com/assignment/entity/Product.java
@@ -0,0 +1,42 @@
+package com.assignment.entity;
+
+public class Product {
+	
+	private int productId;
+	private String productName;
+	private String expDate;
+	public Product() {
+		super();
+	}
+	public Product(int productId, String productName, String expDate) {
+		super();
+		this.productId = productId;
+		this.productName = productName;
+		this.expDate = expDate;
+	}
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getExpDate() {
+		return expDate;
+	}
+	public void setExpDate(String expDate) {
+		this.expDate = expDate;
+	}
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", productName=" + productName + ", expDate=" + expDate + "]";
+	}
+	
+	
+
+}
