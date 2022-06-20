23d1a590cb5fc770598e8d1e0c65cac0c0e39619
PB67966 <PB67966@POOLW42SLPC7061.ind.zensar.com>
2022-06-20 13:40:10 +0530

    War File Generated

--- src/main/java/com/assignment/ProductServiceApplication.java
+++ src/main/java/com/assignment/ProductServiceApplication.java
@@ -3,19 +3,21 @@ package com.assignment;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "testing", "com.assignment" })
-public class ProductServiceApplication {
+public class ProductServiceApplication extends SpringBootServletInitializer{
 
 	public static void main(String... args) {
 		System.out.println("Let's Get Started");
 
 		SpringApplication.run(ProductServiceApplication.class, args);
 
-		System.out.println("Bhetu");
+		System.out.println("Tata!!");
 	}
 
 	@Bean
@@ -24,4 +26,13 @@ public class ProductServiceApplication {
 		return new ModelMapper();
 
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return super.configure(builder);
+	}
+	
+	
+	
 }
