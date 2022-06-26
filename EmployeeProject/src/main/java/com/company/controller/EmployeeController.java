51b6d2c643d225c166e5ab6a904d537fcef5222f
PB67966 <PB67966@POOLW42SLPC7061.ind.zensar.com>
2022-06-26 17:37:00 +0530

    New Project Initiated

---
+++ EmployeeProject/src/main/java/com/company/controller/EmployeeController.java
@@ -0,0 +1,12 @@
+package com.company.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+	@RequestMapping("/")
+	public 
+	
+}
---
+++ EmployeeProject/src/main/java/com/company/entity/Employee.java
@@ -0,0 +1,16 @@
+package com.company.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Employee {
+
+	private int empId;
+	private String empName;
+	private String empDept;
+	
+	
+	
+}
