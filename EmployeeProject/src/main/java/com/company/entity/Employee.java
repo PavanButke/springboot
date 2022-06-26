627ff5243b47d62a84b9012aa5f0b4b23748310c
PB67966 <PB67966@POOLW42SLPC7061.ind.zensar.com>
2022-06-26 22:55:38 +0530

    Connected to MySql

--- EmployeeProject/src/main/java/com/company/entity/Employee.java
+++ EmployeeProject/src/main/java/com/company/entity/Employee.java
@@ -1,17 +1,24 @@
 package com.company.entity;
 
+
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+
+@Entity
 public class Employee {
 
+	@Id
 	private int empId;
 	private String empName;
 	private String empDept;
