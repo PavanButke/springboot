0f15a79c42543be9992f231fd51fd4d68552f915
PB67966 <PB67966@POOLW42SLPC7061.ind.zensar.com>
2022-06-26 22:56:03 +0530

    New

--- EmployeeProject/src/main/java/com/company/EmployeeProjectApplication.java
+++ EmployeeProject/src/main/java/com/company/EmployeeProjectApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.company", "com.zensar"})
+
+//when your packages are subpackage of main package where entry-class is declared then there is no need
+// to use @ComponentScan , bCause @SpringBootApplication internally has @SpringBootConfiguration @EnableAutoConfiguration and @ComponentScan within it
+//@ComponentScan(basePackages = {"com.company", "com.zensar"})
 public class EmployeeProjectApplication {
 
 	public static void main(String[] args) {
