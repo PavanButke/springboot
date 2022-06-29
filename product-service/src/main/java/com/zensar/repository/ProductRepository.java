46bb3e3e2d49e595f18a40cdf70716d50f38a71b
PavanButke <75151054+PavanButke@users.noreply.github.com>
2022-06-29 17:04:30 +0530

    Delete product-service directory

--- product-service/src/main/java/com/zensar/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zensar.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.zensar.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Integer>{
-
-}
--- product-service/src/main/java/com/zensar/services/ProductService.java
+++
@@ -1,8 +0,0 @@
-package com.zensar.services;
-
-import com.zensar.entity.Product;
-
-public interface ProductService {
-
-	Product insertProduct(Product product);  
-}
