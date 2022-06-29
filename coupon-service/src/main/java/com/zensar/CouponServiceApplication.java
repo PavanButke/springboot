128fe7d580da19d42bc478224a7a7d9883228ca8
PavanButke <butkepavan55@gmail.com>
2022-06-29 08:28:21 +0530

    Accessing Copoun Via Gateway

--- coupon-service/src/main/java/com/zensar/CouponServiceApplication.java
+++ coupon-service/src/main/java/com/zensar/CouponServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@RefreshScope
 public class CouponServiceApplication {
 	
 	@Value("test.myLanguage")
