5da1b06bf10d8eba2ac877a6377199d2429b3542
PavanButke <butkepavan55@gmail.com>
2022-07-01 09:27:24 +0530

    commit

---
+++ copoun-security/src/main/java/com/zensar/controllers/TestController.java
@@ -0,0 +1,13 @@
+package com.zensar.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+	@GetMapping("/login")
+	public String login() {
+		return "login.html";
+	}
+}
